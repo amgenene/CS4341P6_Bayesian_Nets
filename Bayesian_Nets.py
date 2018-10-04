b632171fc90594403dcdad8b3ac51cd11254f469
alaza <alazar.genene@gmail.com>
2018-10-04 09:36:51 -0400

    I think I semi parsed it into a list

--- Bayesian_Nets.py
+++ Bayesian_Nets.py
@@ -6,7 +6,7 @@ You will need a way to represent whether each node is a query variable, evidence
 '''
 
 import Node
-import numpy as np
+#import numpy as np
 import sys
 
 
@@ -15,13 +15,28 @@ if __name__ == "__main__":
     with open(input_file_name) as file:
         data = file.readlines()
 
-    counter = 0
     def InterpretFile(file):
+        counter = 1
+        parent_counter = 0
+        parents = []
         for line in file:
-            node_name, node_value = line.split(':')
-            if line[counter] == '[':
-                parents, cpt_values = node_value.split(' ')
+            node_name, node_value, = line.split(':')
+            counter=1
+            if parent_counter > 1:
+                parents.append(']')
+            if node_value[counter] == '[':
+                while node_value[counter] != ']':
+                    parents.append(node_value[counter])
+                    counter += 1
+                    parent_counter += 1
 
+        rp = []
+        print(parents)
+        for p in parents:
+            if p.isdigit() or p == ']' or p == '[':
+                rp.append(p)
+        print(rp)
+    InterpretFile(data)
 
 
 
@@ -37,23 +52,23 @@ if __name__ == "__main__":
 
     out_file = open(sys.argv[2],'w')
 
-    if result:
-        for a in param.list_of_bags:
-            line = [a.name+' ']
-            for b in a.contains:
-                line.append(b.name)
-                line.append(' ')
-            out_file.write(''.join(line))
-            out_file.write('\n')
-            num_of_items = 'number of items: ' + str(len(a.contains)) + '\n'
-            out_file.write(num_of_items)
-            total_weight_capacity = 'total weight: '+str(a.current_load)+' '+'total capacity: '+str(a.capacity)+'\n'
-            out_file.write(total_weight_capacity)
-            wasted = 'wasted capacity: ' + str(a.capacity - a.current_load)+'\n'
-            out_file.write(wasted)
-            out_file.write('\n')
-    else:
-        out_file.write('No solution found')
-
-
-
+    # if result:
+    #     for a in param.list_of_bags:
+    #         line = [a.name+' ']
+    #         for b in a.contains:
+    #             line.append(b.name)
+    #             line.append(' ')
+    #         out_file.write(''.join(line))
+    #         out_file.write('\n')
+    #         num_of_items = 'number of items: ' + str(len(a.contains)) + '\n'
+    #         out_file.write(num_of_items)
+    #         total_weight_capacity = 'total weight: '+str(a.current_load)+' '+'total capacity: '+str(a.capacity)+'\n'
+    #         out_file.write(total_weight_capacity)
+    #         wasted = 'wasted capacity: ' + str(a.capacity - a.current_load)+'\n'
+    #         out_file.write(wasted)
+    #         out_file.write('\n')
+    # else:
+    #     out_file.write('No solution found')
+    #
+    #
+    #
